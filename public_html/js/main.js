f5be08dae9321a8784071ba5c695da7395838763
kiiraklis94 <kiiraklis94@gmail.com>
2016-08-18 13:39:39 +0300

    Streamlined jquery code.
    Put jquery code inside main.js instead of the index.html document.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -14,10 +14,42 @@ var vm = new Vue({
             if (text) {
                 this.places.push({location: text});
                 this.newPlace = '';
+                this.jqueryLoadWeather();
             }
         },
         removeLocation: function (index) {
             this.places.splice(index, 1);
+        },
+        jqueryLoadWeather: function () {
+            $(document).ready(function () {
+                $(".weather-widget").each(function () {
+                    var obj = $(this);
+                    var index = $(obj).attr("data-index");
+                    var loc = $(obj).attr("data-location");
+
+                    $.simpleWeather({
+                        location: loc,
+                        woeid: '',
+                        unit: 'c',
+                        success: function (weather) {
+                            var heading = '<h2>' + weather.city + ', ' + weather.country + '</h2>';
+                            $("#" + index + "-heading").html(heading);
+
+                            var icon = '<img src="' + weather.image + '">';
+                            $("#" + index + "-icon").html(icon);
+
+                            var temp = '<h4>' + weather.temp + ' C' + '</h4>';
+                            $("#" + index + "-temp").html(temp);
+
+                            var currently = '<h4>' + weather.currently + '</h4>';
+                            $("#" + index + "-currently").html(currently);
+                        },
+                        error: function (error) {
+                            $("#" + index + "-heading").html('<h2>' + error + '</h2>');
+                        }
+                    });
+                });
+            });
         }
     }
 });
